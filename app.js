026fc691ce4808c11aed1818ef07240f11323826
myjeong19@naver.com <myjeong19@naver.com>
2023-12-13 15:58:47 +0900

    Feat. chat

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -14,30 +13,17 @@ const messageRouter = require('./routes/message');
 const articleRouter = require('./routes/article');
 
 const app = express();
-=======
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var channelAPIRouter = require("./routes/channelAPI");
-var app = express();
->>>>>>> 17712ceb58971181f8cdd3e6e8e03638e4232c47
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-<<<<<<< HEAD
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin', adminRouter);
@@ -45,11 +31,6 @@ app.use('/article', articleRouter);
 app.use('/channel', channelRouter);
 app.use('/member', memberRouter);
 app.use('/message', messageRouter);
-=======
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/api/channel", channelAPIRouter);
->>>>>>> 17712ceb58971181f8cdd3e6e8e03638e4232c47
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -60,11 +41,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 module.exports = app;
