e61141019db2a72d8709c5f32992108ee47b06a0
ysungwon <hwoarang09@naver.com>
2024-01-26 10:56:54 +0900

    test

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,247 +1,224 @@
+//const constants = require("../common/enum");
+console.log("111");
+//const aes = require("mysql-aes");
+//import * as aes from "mysql-aes";
+console.log("112");
+var addGroupList = [];
+var createGroupObject = {};
 
-
-
-
-//주소록 메뉴 클릭시 전체 회원정보 조회 바인딩
-$("#contacts-tab").click(function(){
-
-	var loginUserToken = localStorage.getItem("userauthtoken");
-
-    //AJAX로  전체 회원정보 조회 데이터 바인딩 처리
-	$.ajax({
-        type:'GET',
-        url:'/api/member/all',
-        headers:{
-            Authorization:`Bearer ${loginUserToken}`
-        },
-        dataType:"json",
-        success:function(result){
-            console.log("모든 사용자 정보 호출결과: ",result);
-
-            if(result.code == 200){
-
-                //최초 UL태그내 LI태그 모두 삭제처리 
-                $(".contacts-list").html("");
-
-                $.each(result.data,function(index,user){
-
-                    var userTag=`<li onClick="fnChatEntry(${user.member_id},'${user.name}',1);">
-                    <a href="#">
-                        <div class="contacts-avatar">
-                            <span class="status busy"></span>
-                            <img src="${user.profile_img_path}" alt="Avatar">
-                        </div>
-                        <div class="contacts-list-body">
-                            <div class="contacts-msg">
-                                <h6 class="text-truncate">${user.name}</h6>
-                                <p class="text-truncate">${user.email}</p>
-                            </div>
-                        </div>
-                        <div class="contacts-list-actions">
-                            <div class="action-block">
-                                <img src="img/dots.svg" alt="Actions">
-                            </div>
-                            <div class="action-list">
-                                <span class="action-list-item">Chat User</span>
-                                <span class="action-list-item">Remove User</span>
-                            </div>
-                        </div>
-                    </a>
-                </li>`;
-
-                 $(".contacts-list").append(userTag);
-
-                });
-
-
-            }else{
-                if(result.code == 400){
-                    alert(result.code);
-                }
-            }
-        },
-        error:function(err){
-            console.log("백엔드 API호출 에러발생:",err);
-        }
-    });
-
+$("#createGroupBtn").click(function () {
+  addGroupList = [];
 });
 
+$("#createGroupBtnFinal").click(function () {
+  var loginUserToken = localStorage.getItem("userauthtoken");
+  var groupName = document.getElementById("createGroupNameInput").value;
+  createGroupObject = {
+    owner: currentUser.email,
+    group_name: groupName,
+    group_image_path: "",
+    member_list: addGroupList.join(", "),
+  };
+  console.log("createGroupObject : ", createGroupObject);
+  $.ajax({
+    type: "POST",
+    url: "/api/channel/create",
+    data: createGroupObject,
+    headers: {
+      Authorization: `Bearer ${loginUserToken}`,
+    },
+    dataType: "json",
 
-//선택 사용자별 채팅방 입장처리 함수 
-function fnChatEntry(memberId,nickName,channelType){
-
-    console.log("채팅방 입장위한 선택 사용자 정보:",memberId,nickName);
-
-    //step1: 채팅방 입장처리하기 
-    var channel ={
-        channelType, //1:일대일채널,2:그룹채널
-        channelId:0, //0이면 일대일채널, 0이상이면 그룹채널 고유번호
-        token:localStorage.getItem("userauthtoken"),
-        targetMemberId:memberId,
-        targetNickName:nickName
-    };
-
-    //해당 채널 유형별 채팅방 입장하기 
-    socket.emit("entryChannel",channel);
-
-    //step2: 채팅 화면 UI 표시하기
-    $(".empty-chat-screen").addClass("d-none");
-	$(".chat-content-wrapper").removeClass("d-none");
-	// $(".users-container .users-list li").removeClass("active-chat");
-	// $(this).addClass("active-chat");
-
-}
-
-
-//서버소켓으로 메시지 보내기
-$("#btnSend").click(function(){
-
-	//임시로 현재 사용자 닉네임 사용-추후 토큰에서 정보 추출하기
-	//현재 접속한 채널 고유번호 조회 
-	var channelId = currentChannel.channel_id;
-
-	//현재 접속자 대화명 조회 
-	var memberId = currentUser.member_id;
-	var nickName = currentUser.name;
-	var profile = currentUser.profile_img_path;
-
-	//입력 메시지 조회 
-	var message = $("#txtMessage").val();
-
-	//서버로 그룹 메시지 발송하기 
-	socket.emit("channelMsg",channelId,memberId,nickName,profile,message);
+    success: function (result) {
+      console.log("success :", result);
+    },
+  });
 });
 
-//서버에서 보내준 메시시 수신 처리하기 
-socket.on("receiveTest",function(nickName,msg,sendDate){
+$("#createGroupAddEmailBtn").click(function () {
+  var email = $("#createGroupAddEmailInput").val();
+  var groupName = document.getElementById("createGroupNameInput").value;
+  console.log("hi, ", email, groupName);
+  var loginUserToken = localStorage.getItem("userauthtoken");
 
-    var msgTag = currentUser.name == nickName ? `<li class='chat-right'>
-									<div class="chat-text-wrapper">
-										<div class='chat-text'>
-											<p>${msg}</p>
-											<div class='chat-hour read'>${sendDate} <span>&#10003;</span></div>
-										</div>
-									</div>
-									<div class='chat-avatar'>
-										<img src="img/user24.png" alt="Quick Chat Admin" />
-										<div class='chat-name'>${nickName}</div>
-									</div>
-								</li>`:`<li class='chat-left'>
-									<div class='chat-avatar'>
-										<img src="img/user21.png" alt="Quick Chat Admin" />
-										<div class='chat-name'>${nickName}</div>
-									</div>
-									<div class="chat-text-wrapper">
-										<div class='chat-text'>
-											<p>${msg}</p>
-											<div class='chat-hour read'>${sendDate} <span>&#10003;</span></div>
-										</div>
-									</div>
-								</li>`;
+  $.ajax({
+    type: "POST",
+    url: "/api/member/add",
+    data: {
+      email: email,
+    },
+    headers: {
+      Authorization: `Bearer ${loginUserToken}`,
+    },
+    dataType: "json",
 
-				//채팅 메시지 템플릿 추가
-				$("#chatHistory").append(msgTag);
-
-				//채팅영역 맨 하단으로 스크롤 이동처리 
-				chatScrollToBottom();
+    success: function (result) {
+      if (result.code == 200) {
+        //var groupName = $("createGroupNameInput").val();
+        addGroupList.push(result.data.email);
+        createGroupObject = {
+          owner: currentUser.email,
+          group_name: groupName,
+          group_image_path: "",
+          member_list: addGroupList.join(", "),
+        };
+        console.log("createGroupObject : ", createGroupObject);
+      }
+    },
+  });
+});
+socket.on("receiveTest", function (data) {
+  console.log("receiveTest!!", data);
+  var className =
+    data.nickName === currentNickName ? "chat-left" : "chat-right";
+  var msgTag = `<li class="${className}">
+    <div class="chat-avatar">
+      <img src="img/user21.png" alt="Quick Chat Admin" />
+      <div class="chat-name">${data.nickName}</div>
+    </div>
+    <div class="chat-text-wrapper">
+      <div class="chat-text">
+        <p>${data.message}</p>
+        <div class="chat-hour read">
+          ${data.sendDate} <span>&#10003;</span>
+        </div>
+      </div>
+    </div>
+  </li>`;
+  $("#chat-box").append(msgTag);
+  //채팅영역 맨 하단으로 스크롤 이동처리
+  chatScrollToBottom();
 });
 
-
+socket.on("receiveChannelMsg", function (data) {
+  console.log("receiveTest!!", data);
+  var className =
+    data.nickName === currentNickName ? "chat-left" : "chat-right";
+  var msgTag = `<li class="${className}">
+    <div class="chat-avatar">
+      <img src=${data.profile} alt="Quick Chat Admin" />
+      <div class="chat-name">${data.nickName}</div>
+    </div>
+    <div class="chat-text-wrapper">
+      <div class="chat-text">
+        <p>${data.message}</p>
+        <div class="chat-hour read">
+          ${data.sendDate} <span>&#10003;</span>
+        </div>
+      </div>
+    </div>
+  </li>`;
+  $("#chat-box").append(msgTag);
+  //채팅영역 맨 하단으로 스크롤 이동처리
+  chatScrollToBottom();
+});
 //채팅방 입장완료 메시지 수신기 정의 기능구현
-socket.on("entryok",function(msg,nickName,channelData){
+socket.on("entryOk", function (msg, nickName, channelData) {
+  var msgTag = `<li class="divider">${msg}</li>`;
 
-	console.log("현재 접속중인 채널 정보 수신:",channelData);
-	//현재 접속한  채널정보  전역변수에 저장하기
-	currentChannel = channelData;
-
-
-	var msgTag = `<li class="divider">${msg}</li>`;
-
-
-	//채팅 메시지 템플릿 추가
-	$("#chatHistory").append(msgTag);
-
-	//채팅영역 맨 하단으로 스크롤 이동처리 
-	chatScrollToBottom();
-
+  currentChannel = channelData;
+  console.log("msgTag : ", msgTag);
+  $("#chat-box").append(msgTag);
 });
 
+$("#contacts-tab").click(function () {
+  var loginUserToken = localStorage.getItem("userauthtoken");
 
-//채팅방 메시시 수신 처리하기 
-socket.on("receiveChannelMsg",function(nickName,profile,msg,sendDate){
+  $.ajax({
+    type: "GET",
+    url: "/api/member/allUser",
+    headers: {
+      Authorization: `Bearer ${loginUserToken}`,
+    },
+    dataType: "json",
 
-    var msgTag = currentUser.name == nickName ? `<li class='chat-right'>
-									<div class="chat-text-wrapper">
-										<div class='chat-text'>
-											<p>${msg}</p>
-											<div class='chat-hour read'>${sendDate} <span>&#10003;</span></div>
-										</div>
-									</div>
-									<div class='chat-avatar'>
-										<img src="${profile}" alt="Quick Chat Admin" />
-										<div class='chat-name'>${nickName}</div>
-									</div>
-								</li>`:`<li class='chat-left'>
-									<div class='chat-avatar'>
-										<img src="${profile}" alt="Quick Chat Admin" />
-										<div class='chat-name'>${nickName}</div>
-									</div>
-									<div class="chat-text-wrapper">
-										<div class='chat-text'>
-											<p>${msg}</p>
-											<div class='chat-hour read'>${sendDate} <span>&#10003;</span></div>
-										</div>
-									</div>
-								</li>`;
+    success: function (result) {
+      console.log("백엔드호출 API 호출 결과 : ", result);
+      if (result.code == 200) {
+        //$(".contacts-list").html("");
+        var data = {
+          memberId: currentUser.member_id,
+          name: currentUser.name,
+          //channelType: constants.CHANNEL_TYPE_ONE_VS_ONE,
+          channelType: 1,
+        };
+        $.each(result.data, function (index, user) {
+          var userTag = `                  <li onClick="fnChatEntry(${data});">
+          <a href="#">
+            <div class="contacts-avatar">
+              <span class="status busy"></span>
+              <img src=${user.profile_img_path} alt="Avatar" />
+            </div>
+            <div class="contacts-list-body">
+              <div class="contacts-msg">
+                <h6 class="text-truncate">${user.name}</h6>
+                <p class="text-truncate">${user.email}</p>
+              </div>
+            </div>
+            <div class="contacts-list-actions">
+              <div class="action-block">
+                <img src="img/dots.svg" alt="Actions" />
+              </div>
+              <div class="action-list">
+                <span class="action-list-item">Chat User</span>
+                <span class="action-list-item">Remove User</span>
+              </div>
+            </div>
+          </a>
+        </li>`;
 
-				//채팅 메시지 템플릿 추가
-				$("#chatHistory").append(msgTag);
-
-				//채팅영역 맨 하단으로 스크롤 이동처리 
-				chatScrollToBottom();
+          $(".contacts-list").append(userTag);
+        });
+      } else {
+        if (result.code == 400) {
+          alert(result.code);
+        }
+      }
+    },
+    error: function (err) {
+      console.log("백엔드호출 API 호출 에러 발생 : ", err);
+    },
+  });
 });
 
+$("#btnSend").click(function () {
+  var channelId = currentChannel.channel_id;
+  var memberId = currentUser.member_id;
+  var nickName = currentUser.name;
+  var profile = currentUser.profile_img_path;
+  var message = $("#messageInputArea").val();
 
+  socket.emit("channelMsg", {
+    channelId,
+    memberId,
+    nickName,
+    message,
+    profile,
+  });
+});
 
+function fnChatEntry(data) {
+  console.log("member_id : ", data.member_id);
+  console.log("name : ", data.name);
+
+  //step1 : 채팅방 입장처리
+  var channel = {
+    channelType, //1 : 일대일, 2 : 그룹채널
+    channelId: 0, //0 : 일대일, 0이상이면 그룹채널 고유번호
+    token: localStorage.getItem("userauthtoken"),
+    targetMemberId: data.member_id,
+    targetNickName: data.name,
+  };
+  console.log("channel : ", channel);
+  socket.emit("entryChannel", { channel });
+  //step2 :
+  $(".empty-chat-screen").addClass("d-none");
+  $(".chat-content-wrapper").removeClass("d-none");
+  //$(".users-container .users-list li").removeClass("active-chat");
+  //$(this).addClass("active-chat");
+}
 
 //채팅영역 스크롤 최하단 이동시키기
 function chatScrollToBottom() {
-	$("#chatScroll").scrollTop($("#chatScroll")[0].scrollHeight);
+  $("#chatScroll").scrollTop($("#chatScroll")[0].scrollHeight);
 }
-
-
-//환경설정 아이콘 클릭시 백엔드에서 프로필정보 조회 바인딩하기
-$("#settings-tab").click(function(){
-
-				//웹브라우저 로컬스토리지에 저장된 사용자인증 JWT토큰정보 추출하기
-				var loginUserToken = localStorage.getItem("userauthtoken");
-
-				$.ajax({
-					type:'GET',
-					url:'/api/member/profile',
-					headers:{
-						Authorization:`Bearer ${loginUserToken}`
-					},
-					dataType:"json",
-					success:function(result){
-						console.log("현재 사용자 정보 호출결과: ",result);
-
-						if(result.code == 200){
-							$("#email").val(result.data.email);
-							$("#member_name").val(result.data.name);
-							$("#telephone").val(result.data.telephone);
-
-							$("#profile_img").attr("src",result.data.profile_img_path);
-		
-						}else{
-							if(result.code == 400){
-								alert(result.code);
-							}
-						}
-					},
-					error:function(err){
-						console.log("백엔드 API호출 에러발생:",err);
-					}
-				});
-});
--- public/js/main.js
+++ public/js/main.js
@@ -1,58 +1,43 @@
 // Loading
-$(function() {
-	$("#loading-wrapper").fadeOut(3000);
+$(function () {
+  $("#loading-wrapper").fadeOut(500);
 });
-
-
 
 // Toggle sidebar
 $("#toggle-sidebar").click(function () {
-	$(".page-wrapper").toggleClass("toggled");
+  $(".page-wrapper").toggleClass("toggled");
 });
-
-
 
 // Hide Empty Chat Screen
 $(".recent-chat-list .chat-cards li").click(function () {
-	$(".empty-chat-screen").addClass("d-none");
-	$(".chat-content-wrapper").removeClass("d-none");
+  $(".empty-chat-screen").addClass("d-none");
+  $(".chat-content-wrapper").removeClass("d-none");
 });
 
 $(".users-container .users-list li").click(function () {
-	$(".empty-chat-screen").addClass("d-none");
-	$(".chat-content-wrapper").removeClass("d-none");
-	$(".users-container .users-list li").removeClass("active-chat");
-	$(this).addClass("active-chat");
+  $(".empty-chat-screen").addClass("d-none");
+  $(".chat-content-wrapper").removeClass("d-none");
+  $(".users-container .users-list li").removeClass("active-chat");
+  $(this).addClass("active-chat");
 });
-
-
 
 // Lets Chat Mobile View
 $(".lets-chat-mobile-view").click(function () {
-	$(".empty-chat-screen").addClass("d-none");
-	$(".chat-content-wrapper").removeClass("d-none");
-	$(".page-wrapper").toggleClass("toggled");
+  $(".empty-chat-screen").addClass("d-none");
+  $(".chat-content-wrapper").removeClass("d-none");
+  $(".page-wrapper").toggleClass("toggled");
 });
 
-
-
 if ($(window).width() < 768) {
-	$(".users-container .users-list li").click(function () {
-		$(".page-wrapper").toggleClass("toggled");
-	});
-};
-
-
+  $(".users-container .users-list li").click(function () {
+    $(".page-wrapper").toggleClass("toggled");
+  });
+}
 
 // Delete User From Group Modal
 $(".stacked-user .delete-user").click(function () {
-	$(this).closest(".stacked-user").remove();
+  $(this).closest(".stacked-user").remove();
 });
-
-
-
-
-
 
 /***********
 ***********
@@ -64,5 +49,5 @@ $(".stacked-user .delete-user").click(function () {
 
 // Tooltip
 $(function () {
-	$('[data-toggle="tooltip"]').tooltip()
-})
+  $('[data-toggle="tooltip"]').tooltip();
+});
